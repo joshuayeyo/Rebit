8c753aa55051028981a13cec81022b569025814d
eunjin <eunjin0321@naver.com>
2024-11-04 21:23:32 +0900

    feat: challenge 생성 버튼 생성  모달 연결

--- src/components/feature/challenge/section/WriteButton/index.tsx
+++ src/components/feature/challenge/section/WriteButton/index.tsx
@@ -1,10 +1,25 @@
 import { Flex } from "@chakra-ui/react";
 import styled from '@emotion/styled';
+import PostChallengyModal from "@/components/feature/modals/challenges/PostChallenge";
+import { useAuth } from '@/provider/Auth';
 
 
-const WriteButton = () => { 
+type Props = {
+  handleModalClose: () => void;
+  isModalOpen: boolean;
+  setIsModalOpen: (visible: boolean) => void;
+}
+
+
+const WriteButton = ({ handleModalClose, isModalOpen, setIsModalOpen }: Props) => { 
+  const { isLogin } = useAuth();
   const handleClick =() => {
-    alert("챌린지 생성")
+    if (!isLogin) {
+      alert('로그인이 필요합니다.');
+      return;
+    }else{
+      setIsModalOpen(true);
+    }
   };
 
   return (
@@ -12,6 +27,12 @@ const WriteButton = () => {
       <Button onClick={handleClick}>
         + 챌린지 생성
       </Button>
+      {isModalOpen &&(
+        <PostChallengyModal
+          isModalOpen={isModalOpen}
+          handleModalClose={handleModalClose}
+        />
+      )}
     </Flex>
   );
 
