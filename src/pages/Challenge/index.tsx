1acecfa5b9e823ba051fbab6a5ea9c97fa2f9494
eunjin <eunjin0321@naver.com>
2024-11-04 21:26:31 +0900

    feat: challenge page item 조회 컴포넌트 연결

--- src/pages/Challenge/index.tsx
+++ src/pages/Challenge/index.tsx
@@ -1,14 +1,14 @@
 import CommonHeader from "@/components/common/Header";
 import Navbar from "@/components/feature/challenge/section/NavBar/";
-import WriteButton
- from "@/components/feature/challenge/section/WriteButton";
+import ChallegeItemSection from "@/components/feature/challenge/section/ChallengeItems";
+
 const ChallengePage = () =>{
   
   return(
     <>
       <CommonHeader />
       <Navbar/>
-      <WriteButton/>
+      <ChallegeItemSection/>
     </>
   )
 }
