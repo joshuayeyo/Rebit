9487e07e7de55ace3fe3c1ede5be5a96ad66dbcc
eunjin <eunjin0321@naver.com>
2024-11-06 16:13:11 +0900

    feat: useChallengeFilter hook 추가

---
+++ src/util/hooks/useChallengeFilter/index.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+
+type FilterType = 'RECRUITING' | 'IN_PROGRESS' | 'COMPLETED' | 'UPCOMING' | 'ALL';
+
+interface ChallengeWithDates {
+  recruitmentStartDate: string;
+  recruitmentEndDate: string;
+  challengeStartDate: string;
+  challengeEndDate: string;
+}
+
+function useChallengeFilter<T extends ChallengeWithDates>(
+  challenges: T[],
+  initialFilter: FilterType = 'ALL'
+) {
+  const [filter, setFilter] = useState<FilterType>(initialFilter);
+  const [filteredData, setFilteredData] = useState<T[]>(challenges);
+
+  useEffect(() => {
+    const updateFilteredData = () => {
+      const currentDate = new Date();
+
+      const filtered = challenges.filter((challenge) => {
+        const recruitmentStart = new Date(challenge.recruitmentStartDate);
+        const recruitmentEnd = new Date(challenge.recruitmentEndDate);
+        const challengeStart = new Date(challenge.challengeStartDate);
+        const challengeEnd = new Date(challenge.challengeEndDate);
+        if (filter === 'UPCOMING'){
+          return currentDate < recruitmentStart
+        }
+        else if (filter === 'RECRUITING') {
+          return currentDate >= recruitmentStart && currentDate <= recruitmentEnd;
+        } else if (filter === 'IN_PROGRESS') {
+          return currentDate >= challengeStart && currentDate <= challengeEnd;
+        } else if (filter === 'COMPLETED') {
+          return currentDate > challengeEnd;
+        }
+      });
+
+      setFilteredData(filtered);
+    };
+
+    updateFilteredData();
+  }, [challenges, filter]);
+
+  useEffect(() => {
+    console.log(`Filtered data for filter type "${filter}":`, filteredData);
+  }, [filter]);
+
+  return { filteredData, setFilter };
+}
+
+export default useChallengeFilter;
