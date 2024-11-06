3c13f558600b1f1e4db164a1ef582cf1b8410f33
eunjin <eunjin0321@naver.com>
2024-11-06 16:18:37 +0900

    feat: challenge 카드 선택시 해당 challengdetail 이동 연결

--- src/components/feature/challenge/section/ChallengeItems/index.tsx
+++ src/components/feature/challenge/section/ChallengeItems/index.tsx
@@ -6,12 +6,22 @@ import { motion, AnimatePresence } from 'framer-motion';
 import instance from '@/api/instance';
 import { Skeleton } from '@chakra-ui/react';
 import ChallengeCard from '../../post/Card';
+import useChallengeFilter from '@/util/hooks/useChallengeFilter';
+import { Link } from 'react-router-dom';
 
-const ChallegeItemSection = () =>{
+type FilterType = 'RECRUITING' | 'IN_PROGRESS' | 'COMPLETED' | 'UPCOMING' | 'ALL';
+
+type ChallegeItemSectionProps = {
+  filterType: FilterType;
+};
+
+const ChallegeItemSection = ({ filterType }: ChallegeItemSectionProps) =>{
   const [isPostModalOpen, setIsPostModalOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
+
   const [data, setData] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const { filteredData, setFilter } = useChallengeFilter(data);
 
   useEffect(() => {
     async function getFeedData() {
@@ -40,13 +50,17 @@ const ChallegeItemSection = () =>{
     document.body.style.overflow = 'auto';
   }
 
+  useEffect(() => {
+    setFilter(filterType);
+  }, [filterType]);
+
   return (
     <Wrapper>
       <Skeleton isLoaded={!isLoading}>
       <CommonGrid columns={4} gap={50}>
         <AnimatePresence>
-          {Array.isArray(data) &&
-                data.map((data) => (
+          {Array.isArray(filteredData) &&
+                filteredData.map((data) => (
                   <motion.div
                     key={data.id}
                     initial={{ opacity: 0, y: 20 }}
@@ -54,14 +68,16 @@ const ChallegeItemSection = () =>{
                     exit={{ opacity: 0, y: -20 }}
                     transition={{ duration: 0.3 }}
                   >
-                    <ItemWrapper>
-                      <ChallengeCard
-                        imageUrl={data.presignedUrl}
-                        title={data.content}
-                        author={data.creator.nickname}
-                        profile={data.creator.prsignedUrl}
-                      />
-                    </ItemWrapper>
+                    <Link to={`/challenge/detail?id=${data.id}&filter=${filterType}`}>
+                      <ItemWrapper>
+                        <ChallengeCard
+                          imageUrl={data.presignedUrl}
+                          title={data.content}
+                          author={data.creator.nickname}
+                          profile={data.creator.prsignedUrl}
+                        />
+                      </ItemWrapper>
+                    </Link>
                 </motion.div>
             ))}
         </AnimatePresence>
