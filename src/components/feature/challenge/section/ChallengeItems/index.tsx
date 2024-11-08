f3181f310c81e4af6321e2d8c608422ca49bbb63
eunjin <eunjin0321@naver.com>
2024-11-08 15:06:42 +0900

    Revert "style: prettier 적용"
    This reverts commit 9e2374c8a9d0ba620cfb80e3405d3a045586c99f.

--- src/components/feature/challenge/section/ChallengeItems/index.tsx
+++ src/components/feature/challenge/section/ChallengeItems/index.tsx
@@ -9,13 +9,18 @@ import ChallengeCard from '../../post/Card';
 import useChallengeFilter from '@/util/hooks/useChallengeFilter';
 import { Link } from 'react-router-dom';
 
-type FilterType = 'RECRUITING' | 'IN_PROGRESS' | 'COMPLETED' | 'UPCOMING' | 'ALL';
+type FilterType =
+  | 'RECRUITING'
+  | 'IN_PROGRESS'
+  | 'COMPLETED'
+  | 'UPCOMING'
+  | 'ALL';
 
 type ChallegeItemSectionProps = {
   filterType: FilterType;
 };
 
-const ChallegeItemSection = ({ filterType }: ChallegeItemSectionProps) =>{
+const ChallegeItemSection = ({ filterType }: ChallegeItemSectionProps) => {
   const [isPostModalOpen, setIsPostModalOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -39,7 +44,6 @@ const ChallegeItemSection = ({ filterType }: ChallegeItemSectionProps) =>{
     getFeedData();
   }, [setData]);
 
-
   const handlePostModalClose = () => {
     setIsPostModalOpen(false);
   };
@@ -57,39 +61,42 @@ const ChallegeItemSection = ({ filterType }: ChallegeItemSectionProps) =>{
   return (
     <Wrapper>
       <Skeleton isLoaded={!isLoading}>
-      <CommonGrid columns={4} gap={50}>
-        <AnimatePresence>
-          {Array.isArray(filteredData) &&
-                filteredData.map((data) => (
-                  <motion.div
-                    key={data.id}
-                    initial={{ opacity: 0, y: 20 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    exit={{ opacity: 0, y: -20 }}
-                    transition={{ duration: 0.3 }}
+        <CommonGrid columns={4} gap={50}>
+          <AnimatePresence>
+            {Array.isArray(filteredData) &&
+              filteredData.map((data) => (
+                <motion.div
+                  key={data.id}
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -20 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <Link
+                    to={`/challenge/detail?id=${data.id}&filter=${filterType}`}
                   >
-                    <Link to={`/challenge/detail?id=${data.id}&filter=${filterType}`}>
-                      <ItemWrapper>
-                        <ChallengeCard
-                          imageUrl={data.presignedUrl}
-                          title={data.content}
-                          author={data.creator.nickname}
-                          profilePics={data.creator.prsignedUrl}
-                        />
-                      </ItemWrapper>
-                    </Link>
+                    <ItemWrapper>
+                      <ChallengeCard
+                        imageUrl={data.presignedUrl}
+                        title={data.content}
+                        author={data.creator.nickname}
+                        profilePics={data.creator.prsignedUrl}
+                      />
+                    </ItemWrapper>
+                  </Link>
                 </motion.div>
-            ))}
-        </AnimatePresence>
-      </CommonGrid>
-      <WriteButton 
-        isModalOpen={isPostModalOpen} 
-        setIsModalOpen={setIsPostModalOpen} 
-        handleModalClose={handlePostModalClose}/>
+              ))}
+          </AnimatePresence>
+        </CommonGrid>
+        <WriteButton
+          isModalOpen={isPostModalOpen}
+          setIsModalOpen={setIsPostModalOpen}
+          handleModalClose={handlePostModalClose}
+        />
       </Skeleton>
     </Wrapper>
-  )
-}
+  );
+};
 
 const Wrapper = styled.section`
   margin-top: 2rem;
