dfd2f9d256f5b4ec234385c4d99c4575410ab2d3
joshuadesu <jeongwongwak@naver.com>
2024-11-09 20:11:37 +0900

    Feat(118): 인생책 상세 페이지를 위한 Route 설정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,58 +1,61 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "@/pages/Home";
-import LoginPage from "@/pages/Login";
-import BookSearchPage from "@/pages/BookSearch";
-import FeedPage from "@/pages/Feed";
-import ChallengePage from "@/pages/Challenge";
-import MyPage from "@/pages/MyPage";
-
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from '@/pages/Home';
+import LoginPage from '@/pages/Login';
+import BookSearchPage from '@/pages/BookSearch';
+import FeedPage from '@/pages/Feed';
+import ChallengePage from '@/pages/Challenge';
+import MyPage from '@/pages/MyPage';
+import BookDetailPage from '@/pages/BookDetail';
 
 // import LoginPage from "@/pages/Login";
 
-import { RouterPath } from "./path";
+import { RouterPath } from './path';
 
 const router = createBrowserRouter([
-    {
-        path: RouterPath.home,
-        element: <HomePage />,
-    },
-    // {
-    //     path: RouterPath.layout,
-    //     element: <Layout />,
-    //     children: [
-    //         {
-    //             path: RouterPath.myPage,
-    //             element: <MyPage />
-    //         },
-    //         {
-    //             path: RouterPath.challenge,
-    //             element: <ChallengeFeedPage />
-    //         }
-    //     ]
-    // },
-    {
-        path: RouterPath.login,
-        element: <LoginPage />,
-    },
-    {
-        path: RouterPath.feed,
-        element: <FeedPage />,
-    },
-    {
-        path: RouterPath.challenge,
-        element: <ChallengePage />,
-    },
-    {
-        path: RouterPath.bookSearch,
-        element: <BookSearchPage />,
-    },
-    {
-        path: RouterPath.myPage,
-        element: <MyPage />
-    },
+  {
+    path: RouterPath.home,
+    element: <HomePage />,
+  },
+  // {
+  //     path: RouterPath.layout,
+  //     element: <Layout />,
+  //     children: [
+  //         {
+  //             path: RouterPath.myPage,
+  //             element: <MyPage />
+  //         },
+  //         {
+  //             path: RouterPath.challenge,
+  //             element: <ChallengeFeedPage />
+  //         }
+  //     ]
+  // },
+  {
+    path: RouterPath.login,
+    element: <LoginPage />,
+  },
+  {
+    path: RouterPath.feed,
+    element: <FeedPage />,
+  },
+  {
+    path: RouterPath.challenge,
+    element: <ChallengePage />,
+  },
+  {
+    path: RouterPath.bookSearch,
+    element: <BookSearchPage />,
+  },
+  // {
+  //     path: RouterPath.myPage,
+  //     element: <MyPage />
+  // },
+  {
+    path: RouterPath.bookdetails,
+    element: <BookDetailPage />,
+  },
 ]);
 
 export const Routes = () => {
-    return<RouterProvider router={router} />
-}
+  return <RouterProvider router={router} />;
+};
